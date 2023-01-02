0b6980736f2e79e409a2bacda62ef6b9f02c0b30
OkOliver20 <104459914+OkOliver20@users.noreply.github.com>
2023-01-02 19:30:18 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function preload() {
+
+}
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+
+    canvas = createCanvas(550, 435);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Inititalized!');
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw() {
+    background('#d3d3d3')
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
